89ebdc878bc9f28123299c0fb594c5d1276bbbdd
gayatpr15 <gayatpr@gmail.com>
2024-03-14 20:40:20 -0400

    Adding Single Creational Pattern

---
+++ DesignPattern/Creational/Singleton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Creational
+{
+    internal class Singleton
+    {
+        private Singleton() { }
+        public static Singleton Instance { get; } = new Singleton();
+
+        public static Singleton GetInstance() {
+            return Singleton.Instance;
+        }
+
+        public void DisplayMessage() {
+            Console.WriteLine("This is Singleton Design Pattern");
+        }
+    }
+}
--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DesignPattern.Creational;
+
 Console.WriteLine("Hello, World!");
+
+Singleton singleton1 = Singleton.GetInstance();
+singleton1.DisplayMessage();
+
+Singleton singleton2 = Singleton.GetInstance();
+singleton2.DisplayMessage();
+
+Console.WriteLine($"Are these instances same? {singleton1 == singleton2}");
